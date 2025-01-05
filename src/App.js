52a660d8daa24ab46ce8ef1c275cf08521b65f18
Evan <estice@ufl.edu>
2025-01-04 17:57:40 -0800

    Initial Map implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
 
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  })
+  const center = { lat: 37.7749, lng: -122.4194 };
+
+  if (!isLoaded) {
+    return <>Not loaded</>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="map-wrapper">
+      <GoogleMap
+      center={center}
+      zoom={10}
+      mapContainerStyle={{ width: '100%', height: '100%' }}
+      options={{
+        zoomControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        fullscreenControl: false
+      }}
+      />
     </div>
   );
 }
